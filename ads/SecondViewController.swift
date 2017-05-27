ac58f0481ce48ce9bb130ee91a27b624234462e8
clare <clare.kang@bitse.com>
2017-05-27 10:27:47 +0800

    Initial Commit

---
+++ ads/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ads
+//
+//  Created by clare on 2017/5/27.
+//  Copyright © 2017年 bitse. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
